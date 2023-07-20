e2e7dddc9c09dd7044b3fbd470577f2054d728b5
Adwaith.k <102245239+adwaith0007@users.noreply.github.com>
2023-07-20 19:06:27 +0530

    db

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,31 @@
 const  express = require("express");
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 
 
 //config 
 
-if(process.env)
+if(process.env.NODE_ENV !== "PRODUCTION")
+{
+    require("dotenv").config({
+        path:"backend/config/.env"
+    }) 
+}
+
+
+// it's for ErrorHandling
+app.use(ErrorHandler);
+
+module.exports = app;
